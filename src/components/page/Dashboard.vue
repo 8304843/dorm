0c97b6220040128aac646b981e757de5ca29e1bb
8304843 <317406241@qq.com>
2019-09-01 10:33:06 +0800

    2019/09/01 10点32分
    第一次上传

---
+++ src/components/page/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="table">
+    <div class="container">
+      <table-list></table-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import TableList from '../page/project/components/MarketList'
+export default {
+  name: "dashboard",
+  components: {
+    Tabs: require("./Tabs.vue").default,
+    TableList
+  },
+  computed: {
+  }
+};
+</script>
+
+<style scoped>
+/* .el-row {
+        margin-bottom: 20px;
+    } */
+
+</style>
