d952b7787e71e517e42b6f3e2fe40e827ea1f191
515354508@qq.com <46087097+515354508@users.noreply.github.com>
2019-09-08 10:05:03 +0800

    2019/9/8 10：04
    异常记录

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import router from './router';
 import Router from 'vue-router'
 import ElementUI from 'element-ui';
 import VueI18n from 'vue-i18n';
+import axios from 'axios';
 import { messages } from './components/common/i18n';
 import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
 // import './assets/css/theme-green/index.css'; // 浅绿色主题
 import './assets/css/icon.css';
 import './components/common/directives';
 import 'babel-polyfill';
+
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false;
 Vue.use(VueI18n);
